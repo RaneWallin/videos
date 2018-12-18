d4c0f3acebaa2ea3080afbecc49d9d92dbd2ef0d
Rane Wallin <RAN2143066@maricopa.edu>
2018-12-18 10:48:07 -0700

    create searchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 
-
-class App extends  Component {
-    render() {
-        return <div>App</div>;
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
