ff8b34962bc4258c2dc0f1ecff9fea09d0914180
Rane Wallin <RAN2143066@maricopa.edu>
2018-12-22 12:21:01 -0700

    create scaffold video list and video item compoentns

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
+import youtube from '../apis/youtube';
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 
 class App extends Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
